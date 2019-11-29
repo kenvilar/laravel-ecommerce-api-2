f5980cc9137010bad8952eb72bb7f6256da3d14f
Ken Vilar <kenvilar@gmail.com>
2019-11-29 16:47:37 +0800

    BuyersController and SellersController implementation

--- app/Http/Controllers/Seller/SellersController.php
+++ app/Http/Controllers/Seller/SellersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Seller;
 
+use App\Models\Seller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,28 +15,9 @@ class SellersController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $sellers = Seller::query()->has('products')->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return response()->json(['data' => $sellers], 200);
     }
 
     /**
@@ -46,40 +28,8 @@ class SellersController extends Controller
      */
     public function show($id)
     {
-        //
-    }
+        $seller = Seller::query()->has('products')->findOrFail($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return response()->json(['data' => $seller], 200);
     }
 }
