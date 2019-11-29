1a44aa0f6dd4340f388597718a51aa22f8cedd5b
Ken Vilar <kenvilar@gmail.com>
2019-11-29 16:35:59 +0800

    Implement index method for BuyersController

--- app/Http/Controllers/Buyer/BuyersController.php
+++ app/Http/Controllers/Buyer/BuyersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Buyer;
 
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +15,9 @@ class BuyersController extends Controller
      */
     public function index()
     {
-        //
+        $buyers = Buyer::query()->has('transactions')->get();
+
+        return response()->json(['data' => $buyers], 200);
     }
 
     /**
