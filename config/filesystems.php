99ac437445813dae8777d177eb0cc73b0b1ba92a
Ken Vilar <kenvilar@gmail.com>
2020-01-12 13:02:31 +0800

    Prepare for the API for images uploading

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,12 @@ return [
             'visibility' => 'public',
         ],
 
+        'images' => [
+            'driver' => 'local',
+            'root' => public_path('img'),
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
