02929504695d23b62b23d1c345398b501e2f1b5c
Ken Vilar <kenvilar@gmail.com>
2020-01-15 13:23:54 +0800

    Minor modification

--- app/Http/Controllers/User/UsersController.php
+++ app/Http/Controllers/User/UsersController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\ApiController;
 use App\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
 class UsersController extends ApiController
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
@@ -24,8 +27,9 @@ class UsersController extends ApiController
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
      */
     public function store(Request $request)
     {
@@ -52,7 +56,7 @@ class UsersController extends ApiController
      * Display the specified resource.
      *
      * @param User $user
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function show(User $user)
     {
@@ -62,10 +66,10 @@ class UsersController extends ApiController
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @param User $user
-     * @return \Illuminate\Http\Response
-     * @throws \Illuminate\Validation\ValidationException
+     * @return JsonResponse
+     * @throws ValidationException
      */
     public function update(Request $request, User $user)
     {
@@ -112,7 +116,7 @@ class UsersController extends ApiController
      * Remove the specified resource from storage.
      *
      * @param User $user
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      * @throws \Exception
      */
     public function destroy(User $user)
@@ -122,7 +126,8 @@ class UsersController extends ApiController
         return $this->showOne($user);
     }
 
-    public function verify($token) {
+    public function verify($token)
+    {
         $user = User::query()->where('verification_token', $token)->firstOrFail();
 
         $user->verified = User::VERIFIED_USER;
