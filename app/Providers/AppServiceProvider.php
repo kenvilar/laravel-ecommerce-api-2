e0321daa04b85943d306959fb5a068394a01d413
Ken Vilar <kenvilar@gmail.com>
2020-01-15 11:26:45 +0800

    Send verification email using events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Mail\UserCreated;
 use App\Models\Product;
+use App\User;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +29,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        User::created(function ($user) {
+            // you can also use Mail::to($user->email)->send(new UserCreated());
+            Mail::to($user)->send(new UserCreated($user));
+        });
+
         Product::updated(function ($product) {
             if ($product->quantity === 0 && $product->isAvailable()) {
                 $product->status = Product::UNAVAILABLE_PRODUCT;
