c525201d2e0b5c9c144ddf8723f48bf027c53191
Ken Vilar <kenvilar@gmail.com>
2020-01-17 12:00:27 +0800

    The Caching System and the Query Parameters

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -103,8 +103,14 @@ trait ApiResponser
     protected function cacheResponse($data)
     {
         $url = request()->url();
+        $queryParams = request()->query();
 
-        return Cache::remember($url, 30, function () use ($data) {
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 30, function () use ($data) {
             return $data;
         });
     }
